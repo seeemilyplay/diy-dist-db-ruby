970b8025628baa31dc95b0c29e9da27782244c34
Emily <emily@skrivapa.se>
2015-06-28 20:23:13 +0100

    add replication factor and write consistency

--- lib/diy-dist-db/dist_db.rb
+++ lib/diy-dist-db/dist_db.rb
@@ -1,9 +1,22 @@
 require_relative 'node'
 
 module DistDB
-    def DistDB.write(node_urls, thing)
-        #todo: only works with one node, need to make distributed!
-        Node.put_thing(node_urls[0], thing)
+    def DistDB.write(node_urls,
+                     replication_factor,
+                     write_consistency,
+                     thing)
+        results = node_urls.take(replication_factor).map { |node_url|
+            begin
+                Node.put_thing(node_url, thing)
+                1
+            rescue Exception
+                0 
+            end
+        } 
+        success_count = results.reduce(:+)
+        if success_count < write_consistency
+            raise "Only wrote to #{success_count} nodes"    
+        end
     end
 
     def DistDB.read(node_urls, id)
--- lib/diy-dist-db/main.rb
+++ lib/diy-dist-db/main.rb
@@ -1,9 +1,11 @@
 require_relative 'dist_db'
 require_relative 'thing'
 
-node_urls = ['http://localhost:8080']
-DistDB.write(node_urls, Thing.new(3, 'foo'))
-DistDB.write(node_urls, Thing.new(7, 'bar'))
+node_urls = ['http://localhost:8080',
+             'http://localhost:8081',
+             'http://localhost:8082']
+DistDB.write(node_urls, 3, 2, Thing.new(3, 'foo'))
+DistDB.write(node_urls, 3, 2, Thing.new(7, 'bar'))
 thing_3 = DistDB.read(node_urls, 3)
 thing_7 = DistDB.read(node_urls, 7)
 puts thing_3
